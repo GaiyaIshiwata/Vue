a0ea455fc27983a6bc2c5c5c379de57beb67c143
touma415 <touma415@gmail.com>
2019-10-29 17:41:03 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <header>
+      <myheader></myheader>
+    </header>
+
+    <p v-if="msg.length > 0">{{msg}}</p>
+    <p v-else>no text</p>
+    <input type="text" v-model="msg" />
+    <button @click="clear()">clear</button>
+
+    <footer>
+      <myfooter></myfooter>
+    </footer>
   </div>
 </template>
 
+
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import myheader from "./components/myheader";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    myheader
+  },
+  data() {
+    return {
+      msg: "Hello World!"
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = "";
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./css/ress.css";
+@import "./css/style.css";
 </style>
---
+++ src/components/myfooter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer-menu">
+    <ul class="menu-list">
+      <li class="menu-item">
+        <i class="fas fa-home"></i>
+      </li>
+      <li class="menu-item">
+        <i class="fas fa-heart"></i>
+      </li>
+      <li class="menu-item">
+        <i class="fas fa-clipboard-list"></i>
+      </li>
+    </ul>
+  </div>
+</template>
